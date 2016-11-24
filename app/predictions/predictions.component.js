188c1b623f0cc709e0999d2e695b55341472da08
Deb Banerji <debkbanerji@gmail.com>
2016-11-24 15:26:07 -0500

    Added Adorable Favicon

--- app/predictions/predictions.component.js
+++ app/predictions/predictions.component.js
@@ -3,7 +3,32 @@ angular.module('predictions').component('predictions', {
 
     controller: ['$routeParams', '$route', '$firebaseObject', '$firebaseArray', function predictionsController($routeParams, $route, $firebaseObject, $firebaseArray) {
         var self = this;
-        var user = firebase.auth().currentUser;
-        self.predictionsRef = firebase.database().ref().child("users").child(user.uid).child("predictions");
+        // var canPredict = false;
+        self.user = firebase.auth().currentUser;
+        self.rangesRef = firebase.database().ref().child("ranges");
+        self.predicted = firebase.database().ref().child("already-predicted");
+        self.thisUserRef = self.predicted.child(self.user.uid);
+
+        self.ranges = $firebaseArray(self.rangesRef);
+        // console.log("PREDICTED");
+
+        self.thisUserObj = $firebaseObject(self.thisUserRef);
+        // console.log(self.thisUserObj);
+
+        self.predict = function(range) {
+            self.thisUserObj.$value = true;
+            self.thisUserObj.$save();
+            // console.log(range.users);
+            // console.log(self.user);
+            if (!range.users) {
+                range.users = [];
+            }
+            range.users.push(self.user.displayName);
+            self.ranges.$save(range).then(function(ref) {
+                // console.log(ref);
+            }, function(error) {
+                // console.log("Error:", error);
+            });
+        }
     }]
 });
